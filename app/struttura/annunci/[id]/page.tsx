b8960f11382789c151ced9503239aed77ec6d12c
luzatrade <lucianozavaglia.lz@gmail.com>
2026-05-14 15:32:27 +0200

    Add hotel offer detail page

---
+++ app/struttura/annunci/[id]/page.tsx
@@ -0,0 +1,9 @@
+import { CreateOfferForm } from "@/components/offers/CreateOfferForm";
+
+export default function Page() {
+  return (
+    <main className="mx-auto max-w-5xl px-4 py-8 sm:px-6 lg:px-8">
+      <CreateOfferForm />
+    </main>
+  );
+}
