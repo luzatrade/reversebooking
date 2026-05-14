18fa8f4717ca3714cd4eccdf41ce46ecadf84dae
luzatrade <lucianozavaglia.lz@gmail.com>
2026-05-14 15:37:47 +0200

    Add public hotel profile page

---
+++ app/hotel/[id]/page.tsx
@@ -0,0 +1,9 @@
+import { PublicHotelProfile } from "@/components/hotels/PublicHotelProfile";
+
+export default function Page() {
+  return (
+    <main className="mx-auto max-w-5xl px-4 py-8 sm:px-6 lg:px-8">
+      <PublicHotelProfile />
+    </main>
+  );
+}
