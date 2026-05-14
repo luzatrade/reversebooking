56522a5ddad3480975409a490b2c60023f637d38
luzatrade <lucianozavaglia.lz@gmail.com>
2026-05-14 15:53:45 +0200

    Add hotel profile edit page

---
+++ app/struttura/profilo/page.tsx
@@ -0,0 +1,9 @@
+import { EditHotelProfileForm } from "@/components/hotels/EditHotelProfileForm";
+
+export default function Page() {
+  return (
+    <main className="mx-auto max-w-5xl px-4 py-8 sm:px-6 lg:px-8">
+      <EditHotelProfileForm />
+    </main>
+  );
+}
