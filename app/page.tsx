91d99a8dfba5566727cb3e7f7ab1de7300bd17ac
luzatrade <lucianozavaglia.lz@gmail.com>
2026-05-14 22:59:23 +0200

    Show advertiser and hotel home alerts

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { HomeAdvertiserAlerts } from "@/components/showcase/HomeAdvertiserAlerts";
 import { HomeHotelAlerts } from "@/components/showcase/HomeHotelAlerts";
 import { PublicShowcaseClient } from "@/components/showcase/PublicShowcaseClient";
 
 export default function HomePage() {
   return (
     <>
+      <HomeAdvertiserAlerts />
       <HomeHotelAlerts />
       <PublicShowcaseClient />
     </>
