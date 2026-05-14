5a0ed40f247304018ae82691ce407b8ffe77e418
luzatrade <lucianozavaglia.lz@gmail.com>
2026-05-14 22:04:58 +0200

    Add shared account settings route

---
+++ app/account/page.tsx
@@ -0,0 +1,9 @@
+import { AccountSettingsClient } from "@/components/account/AccountSettingsClient";
+
+export default function AccountPage() {
+  return (
+    <main className="mx-auto max-w-4xl px-4 py-8 sm:px-6 lg:px-8">
+      <AccountSettingsClient />
+    </main>
+  );
+}
