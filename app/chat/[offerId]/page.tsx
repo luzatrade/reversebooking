ff2cc8f0ecd703fbfd0b4bb986bae7b297b88293
luzatrade <lucianozavaglia.lz@gmail.com>
2026-05-14 18:06:39 +0200

    Add chat route

---
+++ app/chat/[offerId]/page.tsx
@@ -0,0 +1,9 @@
+import { OfferChat } from "@/components/chat/OfferChat";
+
+export default function OfferChatPage() {
+  return (
+    <main className="mx-auto max-w-5xl px-4 py-8 sm:px-6 lg:px-8">
+      <OfferChat />
+    </main>
+  );
+}
