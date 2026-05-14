6bb42d7f4b8060d660fdcc05970c222f3e18492c
luzatrade <lucianozavaglia.lz@gmail.com>
2026-05-15 00:19:50 +0200

    Add mint home design trial wrapper

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import { PublicShowcaseClient } from "@/components/showcase/PublicShowcaseClient
 
 export default function HomePage() {
   return (
-    <>
+    <div className="rb-mint-home">
       <HomeAdvertiserAlerts />
       <HomeHotelAlerts />
       <PublicShowcaseClient />
-    </>
+    </div>
   );
 }
