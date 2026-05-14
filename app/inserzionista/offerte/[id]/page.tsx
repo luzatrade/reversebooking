4fe15ab794d238861c859a6d6efbe2e2828ff0e6
luzatrade <lucianozavaglia.lz@gmail.com>
2026-05-14 15:36:58 +0200

    Add advertiser offer detail page

---
+++ app/inserzionista/offerte/[id]/page.tsx
@@ -0,0 +1,9 @@
+import { AdvertiserOfferDetail } from "@/components/offers/AdvertiserOfferDetail";
+
+export default function Page() {
+  return (
+    <main className="mx-auto max-w-5xl px-4 py-8 sm:px-6 lg:px-8">
+      <AdvertiserOfferDetail />
+    </main>
+  );
+}
